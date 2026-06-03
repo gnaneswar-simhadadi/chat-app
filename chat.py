faf885266249e988ef5c060304e2ff1c362f5604
Gnaneswar Rao <gnaneswar.simhadadi@gmail.com>
2026-06-03 21:39:58 +0530

    security cleanup

--- chat.py
+++ chat.py
@@ -16,4 +16,4 @@ def handle_message(msg):
     send(msg, broadcast=True)
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000, debug=True, allow_unsafe_werkzeug=True)
+    socketio.run(app, debug=False)
