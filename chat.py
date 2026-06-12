31f2f033756aa3060a2f94c9e0ed6da6afcacbea
Gnaneswar Rao <gnaneswar.simhadadi@gmail.com>
2026-06-12 21:02:14 +0530

    Added image sharing feature

--- chat.py
+++ chat.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 from flask_socketio import SocketIO, send
 
@@ -16,4 +17,5 @@ def handle_message(msg):
     send(msg, broadcast=True)
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True, port=5001, allow_unsafe_werkzeug=True)
+    port = int(os.environ.get("PORT",10000))
+    socketio.run(app, host="0.0.0.0", port=port)
