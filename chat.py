bf8b0bd9ae07ad741ea0c5e93a0e6d1b7f595d2c
Gnaneswar Rao <gnaneswar.simhadadi@gmail.com>
2026-06-12 20:36:00 +0530

    Added image sharing feature

--- chat.py
+++ chat.py
@@ -16,4 +16,4 @@ def handle_message(msg):
     send(msg, broadcast=True)
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True, port=5001)
+    socketio.run(app, debug=True, port=5001, allow_unsafe_werkzeug=True)
